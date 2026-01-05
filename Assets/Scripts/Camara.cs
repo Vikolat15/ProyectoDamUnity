478d0fdf1eff319718425ad2b48e0d2a52efb43f
vikolat15 <lagavictor2006@gmail.com>
2026-01-05 15:12:56 +0100

    Primer commit
    Lo logre

---
+++ Assets/Scripts/Camara.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCamara : MonoBehaviour
+{
+    public GameObject personaje;
+    public float suavizado = 0.3f; 
+    public Vector3 offset = new Vector3(0, 0, -10);
+    
+    private Vector3 velocidad = Vector3.zero;
+
+    void Start()
+    {
+       
+        if (personaje != null)
+        {
+            transform.position = new Vector3(
+                personaje.transform.position.x + offset.x,
+                personaje.transform.position.y + offset.y,
+                offset.z
+            );
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (personaje != null)
+        {
+            Vector3 posicionObjetivo = new Vector3(
+                personaje.transform.position.x + offset.x,
+                personaje.transform.position.y + offset.y,
+                offset.z
+            );
+
+
+            transform.position = Vector3.SmoothDamp(
+                transform.position,
+                posicionObjetivo,
+                ref velocidad,
+                suavizado
+            );
+        }
+    }
+}
